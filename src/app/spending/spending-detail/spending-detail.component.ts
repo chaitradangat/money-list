af6e4d276474ea1907ea458b2720bbc7e67892ce
Chaitra Dangat <chaitra_dangat@persistent.com>
2022-12-19 03:14:42 +0530

    Added the spending details component

---
+++ src/app/spending/spending-detail/spending-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-spending-detail',
+  templateUrl: './spending-detail.component.html',
+  styleUrls: ['./spending-detail.component.css']
+})
+export class SpendingDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/spending/spending/spending.module.ts
+++ src/app/spending/spending/spending.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SpendingDetailComponent } from '../spending-detail/spending-detail.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SpendingDetailComponent
+  ],
   imports: [
     CommonModule
-  ]
+  ],
+  exports : [SpendingDetailComponent]
 })
 export class SpendingModule { }
